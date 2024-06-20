5adc055a6b1ae20670c69743ccaf38795cf26876
JamesCharnley <james.anthony.charnley@gmail.com>
2024-06-20 14:04:20 +1200

    Made Article page more responsive

--- src/components/article/Article.js
+++ src/components/article/Article.js
@@ -3,11 +3,24 @@ import styles from './Article.module.css';
 import { ArticleThumbnailSmall } from "../article-thumbnail/ArticleThumbnailSmall";
 import { VerticalBanner } from "../ads/VerticalBanner";
 import { NavBar } from "../navbar/NavBar";
+import { useEffect, useState } from "react";
 
 
 export function Article({articles}) {
 
   let { id } = useParams();
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+  useEffect(() => {
+    const handleResize = () => {
+      setScreenWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
   console.log(id);
   const article = articles.find((a) => a.id === Number(id));
   if(!article)
@@ -19,16 +32,49 @@ export function Article({articles}) {
   return (
     <div className={styles.container}>
       <NavBar />
-      <div className={styles.flex_container}>
-        <div className={styles.flex_row_side}>
-          <VerticalBanner imageUrl={"/sportad.png"} />
-        </div>
-        <div className={styles.article}>
+      <div
+        className={
+          screenWidth > 700
+            ? styles.flex_container
+            : styles.flex_container && styles.flex_row
+        }
+      >
+        {screenWidth > 1600 && (
+          <div className={styles.flex_row_side}>
+            <VerticalBanner imageUrl={"/sportad.png"} />
+          </div>
+        )}
+        <div
+          style={screenWidth > 700 ? { width: "50%" } : { width: "90%" }}
+          className={styles.article}
+        >
           <article>
-            <h1>{article.title}</h1>
+            <h1
+              style={
+                screenWidth > 700 ? { fontSize: "32px" } : { fontSize: "28px" }
+              }
+            >
+              {article.title}
+            </h1>
             <span>
-              <p>Author: {article.author}</p>
-              <p>Published: {date}</p>
+              <p
+                style={
+                  screenWidth > 700
+                    ? { fontSize: "18px" }
+                    : { fontSize: "14px" }
+                }
+              >
+                Author: {article.author}
+              </p>
+              <p
+                style={
+                  screenWidth > 700
+                    ? { fontSize: "18px" }
+                    : { fontSize: "14px" }
+                }
+              >
+                Published: {date}
+              </p>
             </span>
             <div
               style={{ backgroundImage: `url(${article.imageUrl})` }}
@@ -37,8 +83,10 @@ export function Article({articles}) {
             <p dangerouslySetInnerHTML={{ __html: article.content }} />
           </article>
         </div>
-        <div className={styles.flex_row_side}>
-          {articles.map((a) => <ArticleThumbnailSmall article={a} key={a.id} />)}
+        <div style={screenWidth > 700 ? {width: "24%"} : {width: "90%"}} className={styles.flex_row_side}>
+          {articles.map((a) => (
+            <ArticleThumbnailSmall article={a} key={a.id} />
+          ))}
         </div>
       </div>
     </div>
