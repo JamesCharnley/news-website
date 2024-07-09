c85031c682ecda4ed18a657609ba9bba251ede1b
JamesCharnley <james.anthony.charnley@gmail.com>
2024-07-10 00:32:06 +1200

    Responsive text size for thumbnails

--- src/components/article-thumbnail/ArticleThumbnailSmall.js
+++ src/components/article-thumbnail/ArticleThumbnailSmall.js
@@ -1,18 +1,46 @@
+import { useEffect, useRef, useState } from 'react';
 import styles from './ArticleThumbnailSmall.module.css';
 
 
 export function ArticleThumbnailSmall({article}){
+
+  const ref = useRef();
+
+  const [componentWidth, setComponentWidth] = useState(0);
+  useEffect(() => {
+    if(ref.current){
+      setComponentWidth(ref.current.offsetWidth);
+    }
+    const handleResize = () => {
+      if(ref.current){
+        setComponentWidth(ref.current.offsetWidth);
+      }
+      
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+  const defComponentWidth = 360;
+  const defFontSize = 22;
+  const componentWidthRatio = componentWidth / defComponentWidth;
+
   return (
-    <div className={styles.container}>
+    <div className={styles.container} ref={ref}>
       <div
         style={{ backgroundImage: `url(${article.imageUrl})` }}
         className={styles.image}
       ></div>
       <div className={styles.info_container}>
         <a href={`/article/${article.id}`}>
-        <h2 className={styles.title}>
-          {article.title.length > 64 ? article.title.substr(0, 60) + "..." : article.title}
-        </h2>
+          <h2 style={{fontSize: `${defFontSize * componentWidthRatio}px`}} className={styles.title}>
+            {article.title.length > 64
+              ? article.title.substr(0, 60) + "..."
+              : article.title}
+          </h2>
         </a>
         <div className={styles.footer}>
           <a href={`/category/${article.category}`}>{article.category}</a>
--- src/components/article-thumbnail/ArticleThumnailMedium.js
+++ src/components/article-thumbnail/ArticleThumnailMedium.js
@@ -1,15 +1,41 @@
+import { useEffect, useRef, useState } from 'react';
 import styles from './ArticleThumbnailMedium.module.css';
 
 export function ArticleThumbnailMedium({article}){
+
+  const ref = useRef();
+
+  const [componentWidth, setComponentWidth] = useState(0);
+  useEffect(() => {
+    if (ref.current) {
+      setComponentWidth(ref.current.offsetWidth);
+    }
+    const handleResize = () => {
+      if (ref.current) {
+        setComponentWidth(ref.current.offsetWidth);
+      }
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+  const defComponentWidth = 250;
+  //360 = 21, 160 = > 21
+  const defFontSize = componentWidth < 170 ? 25 : componentWidth < 360 ? 21 : 18;
+  const componentWidthRatio = componentWidth / defComponentWidth;
+  console.log(componentWidth);
   return (
-    <div className={styles.container}>
+    <div className={styles.container} ref={ref}>
       <div
         style={{ backgroundImage: `url(${article.imageUrl})` }}
         className={styles.image}
       ></div>
       <div className={styles.info_container}>
         <a href={`/article/${article.id}`}>
-          <h3 className={styles.title}>
+          <h3 style={{fontSize: `${defFontSize * componentWidthRatio}px`}} className={styles.title}>
             {article.title.length > 64
               ? article.title.substr(0, 60) + "..."
               : article.title}
